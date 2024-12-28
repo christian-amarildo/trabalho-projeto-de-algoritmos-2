5d4a23e298965debbb2870f6931dffafeeef6ab1
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2024-12-27 23:29:57 -0300

    Update main.cpp

--- questao1/main.cpp
+++ questao1/main.cpp
@@ -4,52 +4,56 @@
 
 using namespace std;
 
-int main(void) {
-  int op;
-  int valor;
-  t_arvore raiz = NULL;
+int main(void){
+    int op;
+    int valor;
+    bool ret = false;
+    t_arvore raiz = nullptr;
 
-  while (1) {
-    cout << "1 - Inserir valor\n";
-    cout << "2 - Remover valor\n";
-    cout << "3 - Consultar valor\n";
-    cout << "4 - Mostrar a arvore\n";
-    cout << "5 - Sair\n";
-    cout << "Opcao? ";
-    cin >> op;
-
-    switch (op) {
-      case 1: // Inserir valor
-        cout << "Qual o valor para ser inserido na arvore? ";
-        cin >> valor;
-        raiz = inserir(raiz, valor);
-        cout << "Valor inserido com sucesso!" << endl;
-        break;
-
-      case 2: // Remover valor
-        cout << "Qual o valor para ser removido da arvore? ";
-        cin >> valor;
-        raiz = remover(raiz, valor);
-        cout << "Valor removido com sucesso!" << endl;
-        break;
-
-      case 3: // Consultar valor
-        cout << "Qual o valor para pesquisar na arvore? ";
-        cin >> valor;
-        if (pesquisar(raiz, valor)) {
-          cout << "Valor encontrado!" << endl;
-        } else {
-          cout << "Valor nao encontrado!" << endl;
+    while(true){
+        cout << "1 - Inserir valor\n";
+        cout << "2 - Remover valor\n";
+        cout << "3 - Consultar valor\n";
+        cout << "4 - Mostrar a arvore\n";
+        cout << "5 - Sair\n";
+        cout << "Opcao? ";
+        cin >> op;
+        switch(op){
+            case 1: // Inserir valor
+                cout << "Qual o valor para ser inserido na arvore? ";
+                cin >> valor;
+                if (inserir(&raiz, valor)){
+                    cout << "Valor inserido com sucesso: " << valor << endl << endl;
+                }
+                else
+                    cout << "Valor nao inserido com sucesso.\n\n";
+                break;
+            case 2: // Remover valor
+                cout << "Qual o valor para ser removido da arvore? ";
+                cin >> valor;
+                if (remover(&raiz, valor)){
+                    cout << "Valor removido com sucesso: " << valor << endl << endl;
+                }
+                else
+                    cout << "Valor nao encontrado na arvore.\n\n";
+                break;
+            case 3: // Consultar valor
+                cout << "Qual o valor para consultar na arvore? ";
+                cin >> valor;
+                ret = pesquisar(raiz, valor);
+                if (ret)
+                    cout << "Valor " << valor << " encontrado na arvore.\n\n";
+                else
+                    cout << "Valor " << valor << " nao encontrado na arvore.\n\n";
+                break;
+            case 4: // Mostrar a arvore
+                mostrararvore(raiz);
+                break;
+            case 5: // Abandonar o programa
+                // Opcional: Implementar uma função para liberar a memória da árvore
+                exit(0);
+            default:
+                cout << "Opcao invalida!\n\n";
         }
-        break;
-
-      case 4: // Mostrar a árvore
-        mostrararvore(raiz);
-        break;
-
-      case 5: // Sair
-        exit(0);
-        break;
     }
-  }
 }
